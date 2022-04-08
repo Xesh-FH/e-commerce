159c8717cbb463cf632c61949d874fb14c12d3ab
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-04-08 23:16:49 +0200

    Add - Taxes Calculator Service

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+use App\Taxes\Calculator;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HelloController extends AbstractController
 {
+
+    protected LoggerInterface $logger;
+    protected $calculator;
+
+    public function __construct(LoggerInterface $logger, Calculator $calculator)
+    {
+        $this->logger = $logger;
+        $this->calculator = $calculator;
+    }
+
     /**
      * @Route("/hello/{prenom?World}", name="hello", methods={"GET","POST"}, host="localhost", schemes={"http","https"})
      */
     public function helloWorld(string $prenom)
     {
+        $this->logger->info("Message de log pour tester le loggerInterface");
+        $tva = $this->calculator->calcul(100);
+        dump($tva);
         return new Response("Hello " . $prenom);
     }
 }
