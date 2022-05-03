16387bfe8fd9f61ab6f85c76293e46122022577d
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-05-03 14:39:25 +0200

    Add - Product entity + migration

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
