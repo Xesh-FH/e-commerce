cdb80f53d5ed4daaf948f7d0dd24e080caa1ea5c
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-05-04 14:35:30 +0200

    Add - Interpolation
    Affichage lié aux données en base

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,12 +14,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage(EntityManagerInterface $em)
+    public function homepage(ProductRepository $productRepository)
     {
-        $productRepo = $em->getRepository(Product::class);
-
-        $product = $productRepo->find(3);
-
-        return $this->render('home.html.twig');
+        $products = $productRepository->findBy([], [], 3);
+        return $this->render('home.html.twig', [
+            'products' => $products
+        ]);
     }
 }
