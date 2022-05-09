d00065649727f832822d70dfaefec283c8b0fe6f
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-05-09 13:31:43 +0200

    Add - Category Page

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/{slug}", name="product_category")
+     */
+    public function category(string $slug, CategoryRepository $categoryRepository): Response
+    {
+        $category = $categoryRepository->findOneBy([
+            "slug" => $slug
+        ]);
+
+        if (!$category) {
+            throw $this->createNotFoundException("La catégorie $slug n'existe pas");
+        }
+
+        return $this->render('product/category.html.twig', [
+            'slug' => $slug,
+            'category' => $category,
+        ]);
+    }
+}
