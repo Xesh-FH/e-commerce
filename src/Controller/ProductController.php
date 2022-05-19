0ae1cc5e191eda75ef043882f7644e3fa4101445
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-05-19 13:20:59 +0200

    Add - Product creation form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -45,6 +46,29 @@ class ProductController extends AbstractController
 
         return $this->render("product/show.html.twig", [
             "product" => $product,
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/product/create", name="product-create")
+     */
+    public function create(FormFactoryInterface $factory)
+    {
+        $builder = $factory->createBuilder();
+
+        $builder
+            ->add('name')
+            ->add('shortDescription')
+            ->add('price')
+            ->add('category');
+
+        $form = $builder->getForm();
+
+        $formView = $form->createView();
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView,
         ]);
     }
 }
