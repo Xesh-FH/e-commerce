aa6c8c106e566bae3b165ff8adc594f073d0a06e
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-05-20 20:22:50 +0200

    Add - ProductType
    On refacto la création du formulaire en la sortant du controller pour la mettre dans une classe dédiée

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,19 +2,12 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -70,45 +63,7 @@ class ProductController extends AbstractController
         SluggerInterface $slugger,
         EntityManagerInterface $em
     ) {
-        $builder = $factory->createBuilder(FormType::class, null, [
-            'data_class' => Product::class,
-        ]);
-
-        $builder
-            ->add('name', TypeTextType::class, [
-                'label' => 'Nom du produit',
-                'attr' => [
-                    'placeholder' => 'Saisissez le nom du produit à créer',
-                ]
-            ])
-            ->add('shortDescription', TextareaType::class, [
-                'label' => 'Description courte',
-                'attr' => [
-                    'placeholder' => 'Décrivez succintement votre produit de manière assez claire pour le visiteur',
-                ]
-            ])
-            ->add('price', MoneyType::class, [
-                'currency' => true,
-                'label' => 'Prix de vente du produit',
-                'attr' => [
-                    'placeholder' => 'Indiquez le prix de vente TTC du produit en Euros',
-                ]
-            ])
-            ->add('mainPicture', UrlType::class, [
-                'label' => 'Image de présentation du produit',
-                'attr' => [
-                    'placeholder' => 'Renseignez l\'URL de l\'image',
-                ]
-            ])
-            ->add('category', EntityType::class, [
-                'label' => 'Catégorie',
-                'placeholder' => '-- Dans quelle catégorie entre votre produit ? --',
-                'class' => Category::class,
-                'choice_label' => function (Category $category) {
-                    return strtoupper($category->getName());
-                },
-            ]);
-
+        $builder = $factory->createBuilder(ProductType::class);
         $form = $builder->getForm();
         $form->handleRequest($request);
 
---
+++ src/Form/ProductType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\{MoneyType, TextareaType, TextType as TextType, UrlType};
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TypeTextType::class, [
+                'label' => 'Nom du produit',
+                'attr' => [
+                    'placeholder' => 'Saisissez le nom du produit à créer',
+                ]
+            ])
+            ->add('shortDescription', TextareaType::class, [
+                'label' => 'Description courte',
+                'attr' => [
+                    'placeholder' => 'Décrivez succintement votre produit de manière assez claire pour le visiteur',
+                ]
+            ])
+            ->add('price', MoneyType::class, [
+                'currency' => true,
+                'label' => 'Prix de vente du produit',
+                'attr' => [
+                    'placeholder' => 'Indiquez le prix de vente TTC du produit en Euros',
+                ]
+            ])
+            ->add('mainPicture', UrlType::class, [
+                'label' => 'Image de présentation du produit',
+                'attr' => [
+                    'placeholder' => 'Renseignez l\'URL de l\'image',
+                ]
+            ])
+            ->add('category', EntityType::class, [
+                'label' => 'Catégorie',
+                'placeholder' => '-- Dans quelle catégorie entre votre produit ? --',
+                'class' => Category::class,
+                'choice_label' => function (Category $category) {
+                    return strtoupper($category->getName());
+                },
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
