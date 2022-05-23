043b02c71c9657ca0af074a9bb6b5d83bbaa43fb
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-05-23 13:58:27 +0200

    Upd - redirect
    On raccourci encore avec la methode redirect() de l'abstractController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,7 +10,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -85,8 +84,7 @@ class ProductController extends AbstractController
                 'slug' => $product->getSlug(),
             ]);
 
-            $response = new RedirectResponse($url);
-            return $response;
+            return $this->redirect($url);
         }
 
         return $this->render('product/edit.html.twig', [
