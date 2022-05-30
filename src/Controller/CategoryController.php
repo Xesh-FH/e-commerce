ba731fece3ca024d0b1bfcc455c0a89ff0088472
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-05-30 13:54:00 +0200

    Add - Create and edit categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,29 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CategoryController extends AbstractController
 {
     /**
      * @Route("/admin/category/create", name="category_create", methods={"GET", "POST"})
      */
-    public function create(): Response
+    public function create(EntityManagerInterface $em, Request $request, SluggerInterface $slugger): Response
     {
-        return $this->render('category/create-category.html.twig', []);
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $formView = $form->createView();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category->setSlug(strtolower($slugger->slug($category->getName())));
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('category/create-category.html.twig', [
+            'formView' => $formView,
+        ]);
     }
 
     /**
      * @Route("/admin/category/{id}/edit", name="category_edit", methods={"GET", "POST"})
      */
-    public function edit($id, CategoryRepository $categoryRepository): Response
+    public function edit($id, CategoryRepository $categoryRepository, Request $request, SluggerInterface $slugger, EntityManagerInterface $em): Response
     {
         $category = $categoryRepository->find($id);
+        $form = $this->createForm(CategoryType::class, $category);
+        $formView = $form->createView();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category->setSlug(strtolower($slugger->slug($category->getName())));
+
+            $em->flush($category);
+
+            return $this->redirectToRoute('homepage');
+        }
+
         return $this->render('category/edit-category.html.twig', [
             'category' => $category,
+            'formView' => $formView,
         ]);
     }
 }
