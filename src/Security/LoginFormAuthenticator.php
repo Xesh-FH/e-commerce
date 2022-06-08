4418b2221f8dc4cf9b358e3b25788cb4fe2d1bfd
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-06-08 14:18:50 +0200

    Upd - LoginFormAuthenticator
    On met en place la logique de gestion de réussite et d'échec d'authentification.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,15 +2,25 @@
 
 namespace App\Security;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 
 class LoginFormAuthenticator extends AbstractGuardAuthenticator
 {
+    protected UserPasswordEncoderInterface $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function supports(Request $request)
     {
         return $request->attributes->get('_route') === 'security_login'
@@ -19,27 +29,27 @@ class LoginFormAuthenticator extends AbstractGuardAuthenticator
 
     public function getCredentials(Request $request)
     {
-        // todo
+        return $request->request->get('login');
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // todo
+        return $userProvider->loadUserByUsername($credentials['email']);
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // todo
+        return $this->encoder->isPasswordValid($user, $credentials['password']);
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
-        // todo
+        $request->attributes->set(Security::AUTHENTICATION_ERROR, $exception);
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
-        // todo
+        return new RedirectResponse('/');
     }
 
     public function start(Request $request, AuthenticationException $authException = null)
