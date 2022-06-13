060c81d0dc99371ae456df6d2434e532af8c3f55
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-06-13 14:01:48 +0200

    Add - CategoryVoter

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,13 +7,10 @@ use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CategoryController extends AbstractController
@@ -72,14 +69,7 @@ class CategoryController extends AbstractController
             throw new NotFoundHttpException("Cette catégorie n'existe pas.");
         }
 
-        $user = $this->getUser();
-        if (!$user) {
-            return $this->redirectToRoute("security_login");
-        }
-
-        if ($user !== $category->getOwner()) {
-            throw new AccessDeniedHttpException("Vous n'êtes pas le créateur de cette catégorie");
-        }
+        $this->denyAccessUnlessGranted('CAN_EDIT', $category, "Vous n'êtes pas l'auteur de cette catégorie.");
 
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
