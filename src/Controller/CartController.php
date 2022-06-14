4561de7d3e0b2a41673d38d2924fcc338cde16ae
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-06-14 13:36:04 +0200

    Add - CartController
    On ajoute une clé 'cart' à la session

---
+++ src/Controller/CartController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/cart/add/{id}", name="cart_add", requirements= {"id":"\d+"})
+     * @param int $id l'id du Product à ajouter au Cart
+     */
+    public function add(int $id, Request $request, ProductRepository $productRepository): Response
+    {
+        // 0. Sécurisation : est-ce que le produit existe ?
+        $product = $productRepository->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException("Aucun produit avec l'id $id n'existe.");
+        }
+
+        //Le panier $cart aura la forme d'un tableau clé => valeur associant l'id d'un produit à sa quantité dans le panier.
+
+        // 1. Retrouver le panier dans la session (sous forme de tableau)
+        // 2. S'il n'existe pas, prendre un tableau vide
+        $cart = $request->getSession()->get('cart', []);
+
+        // 3. voir si le produit $id existe déjà dans le tableau
+        // 4. Si c'est le cas, simplement augmenter la quantité
+        // 5. Sinon ajouter le produit $id avec la quantité 1
+        if (array_key_exists($id, $cart)) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        // 6. Enregistrer le tableau à jour dans la session
+        $request->getSession()->set('cart', $cart);
+
+        return $this->redirectToRoute('product_show', [
+            'category_slug' => $product->getCategory()->getSlug(),
+            'slug' => $product->getSlug(),
+        ]);
+    }
+}
