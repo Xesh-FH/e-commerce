1ede03f49c3277a7cb151b4622ebbaa8671614de
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-06-15 13:39:20 +0200

    Add - Affichage Flashbag dans twig

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,11 +3,11 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CartController extends AbstractController
 {
@@ -40,6 +40,11 @@ class CartController extends AbstractController
 
         // 6. Enregistrer le tableau à jour dans la session
         $session->set('cart', $cart);
+
+        /** @var FlashBag */
+        $flashBag = $session->getBag('flashes');
+        $flashBag->add('success', "Produit ajouté au panier.");
+
         return $this->redirectToRoute('product_show', [
             'category_slug' => $product->getCategory()->getSlug(),
             'slug' => $product->getSlug(),
