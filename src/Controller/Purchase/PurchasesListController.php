acab689adf799cd66f343e5a6fb7819628c37cbb
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-06-23 13:17:23 +0200

    Upd - Purchases
    Controller Optimization

--- src/Controller/Purchase/PurchasesListController.php
+++ src/Controller/Purchase/PurchasesListController.php
@@ -3,31 +3,21 @@
 namespace App\Controller\Purchase;
 
 use App\Entity\User;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class PurchasesListController extends AbstractController
 {
-    protected Security $security;
-
-    public function __construct(Security $security)
-    {
-        $this->security = $security;
-    }
-
     /**
      * @Route("/purchases", name="purchase_index")
+     * @IsGranted("ROLE_USER", message="Vous devez être connecté pour accéder à vos commandes.")
      */
     public function index(): Response
     {
         /** @var User */
-        $user = $this->security->getUser();
-        if (!$user) {
-            throw new AccessDeniedException("Vous devez être connecté pour accéder à vos commandes.");
-        }
+        $user = $this->getUser();
 
         return $this->render('purchase/purchase_index.html.twig', [
             "purchases" => $user->getPurchases(),
