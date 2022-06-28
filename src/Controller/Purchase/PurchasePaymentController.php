368cb38305dfb1fa743a6fb0e1eef7473888916d
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-06-28 07:23:23 +0200

    Add - Payment with redirect

--- src/Controller/Purchase/PurchasePaymentController.php
+++ src/Controller/Purchase/PurchasePaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Purchase;
 
+use App\Repository\PurchaseRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -10,8 +11,23 @@ class PurchasePaymentController extends AbstractController
     /**
      * @Route("/purchase/pay/{id}", name="purchase_payment_form")
      */
-    public function showPaymentCardForm()
+    public function showPaymentCardForm(int $id, PurchaseRepository $purchaseRepository)
     {
-        return $this->render('purchase/payment.html.twig');
+        $purchase = $purchaseRepository->find($id);
+
+        if (!$purchase) {
+            return $this->redirectToRoute('cart_show');
+        }
+
+        \Stripe\Stripe::setApiKey('sk_test_51LFNFdAICnQ5vYlt3ZGYoB8YnWi2WjaoZ9wEmygsQ6HKdM9gJpaUYJ4ViCS5arvGOQmYBdjSVsmQZXWSP2Y3MKYT00eGXorTX5');
+        $intent = \Stripe\PaymentIntent::create([
+            'amount' => $purchase->getTotal(),
+            'currency' => 'eur',
+        ]);
+
+        return $this->render('purchase/payment.html.twig', [
+            'clientSecret' => $intent->client_secret,
+            'purchase' => $purchase,
+        ]);
     }
 }
---
+++ src/Controller/Purchase/PurchasePaymentSuccessController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller\Purchase;
+
+use App\Entity\Purchase;
+use App\Cart\CartService;
+use App\Repository\PurchaseRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PurchasePaymentSuccessController extends AbstractController
+{
+    /**
+     * @Route("/purchase/validation/{id}", name="purchase_payment_success")
+     * IsGranted("ROLE_USER")
+     */
+    public function paymentSuccess(int $id, PurchaseRepository $purchaseRepository, EntityManagerInterface $em, CartService $cartService)
+    {
+        $purchase = $purchaseRepository->find($id);
+
+        if (!$purchase) {
+            $this->addFlash('warning', [
+                'title' => "Commande erronnée.",
+                'content' => "Cette commande n'existe pas.",
+            ]);
+            return $this->redirectToRoute("purchase_index");
+        }
+        if ($purchase->getUser() !== $this->getUser()) {
+            $this->addFlash('warning', [
+                'title' => "Commande erronnée.",
+                'content' => "Cette commande n'est pas liée à ce compte.",
+            ]);
+            return $this->redirectToRoute("purchase_index");
+        }
+        if ($purchase->getStatus() === Purchase::STATUS_PAYED) {
+            $this->addFlash('warning', [
+                'title' => "Commande erronnée.",
+                'content' => "Cette commande a déjà été payée.",
+            ]);
+            return $this->redirectToRoute("purchase_index");
+        }
+        $purchase->setStatus(Purchase::STATUS_PAYED);
+        $em->flush();
+
+        $cartService->emptyCart();
+
+        $this->addFlash('success', [
+            'title' => "Paiment effectué.",
+            'content' => "La commande a été payée et confirmée.",
+        ]);
+
+        return $this->redirectToRoute("purchase_index");
+    }
+}
