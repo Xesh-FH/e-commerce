8be30482860635aa64784522daa23bb2b9dd2214
Emmanuel <emmanuel.s.bourgeois@gmail.com>
2022-07-05 13:56:05 +0200

    Upd - Stripe
    Refoacto Stripe et clés dans le services.yaml

--- src/Controller/Purchase/PurchasePaymentController.php
+++ src/Controller/Purchase/PurchasePaymentController.php
@@ -2,28 +2,46 @@
 
 namespace App\Controller\Purchase;
 
+use App\Entity\Purchase;
 use App\Repository\PurchaseRepository;
+use App\Stripe\StripeService;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PurchasePaymentController extends AbstractController
 {
     /**
      * @Route("/purchase/pay/{id}", name="purchase_payment_form")
+     * @IsGranted("ROLE_USER")
      */
-    public function showPaymentCardForm(int $id, PurchaseRepository $purchaseRepository)
+    public function showPaymentCardForm(int $id, PurchaseRepository $purchaseRepository, StripeService $stripeService)
     {
         $purchase = $purchaseRepository->find($id);
 
         if (!$purchase) {
-            return $this->redirectToRoute('cart_show');
+            $this->addFlash('warning', [
+                'title' => "Commande erronnée.",
+                'content' => "Cette commande n'existe pas.",
+            ]);
+            return $this->redirectToRoute("purchase_index");
+        }
+        if ($purchase->getUser() !== $this->getUser()) {
+            $this->addFlash('warning', [
+                'title' => "Commande erronnée.",
+                'content' => "Cette commande n'est pas liée à ce compte.",
+            ]);
+            return $this->redirectToRoute("purchase_index");
+        }
+        if ($purchase->getStatus() === Purchase::STATUS_PAYED) {
+            $this->addFlash('warning', [
+                'title' => "Commande erronnée.",
+                'content' => "Cette commande a déjà été payée.",
+            ]);
+            return $this->redirectToRoute("purchase_index");
         }
 
-        \Stripe\Stripe::setApiKey('sk_test_51LFNFdAICnQ5vYlt3ZGYoB8YnWi2WjaoZ9wEmygsQ6HKdM9gJpaUYJ4ViCS5arvGOQmYBdjSVsmQZXWSP2Y3MKYT00eGXorTX5');
-        $intent = \Stripe\PaymentIntent::create([
-            'amount' => $purchase->getTotal(),
-            'currency' => 'eur',
-        ]);
+        $intent = $stripeService->getPaymentIntent($purchase);
 
         return $this->render('purchase/payment.html.twig', [
             'clientSecret' => $intent->client_secret,
